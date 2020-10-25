8969c06527ea7acb932f763d13256c14c400926c
Georgi Petkov <petkovgogo@gmail.com>
2020-10-25 13:27:51 +0200

    Added test function for the sorting result

--- src/GroupSort.cpp
+++ src/GroupSort.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
+//TODO counting sort
+int* countingSort(int* arr, int &size) {return arr;};
+//TODO selection sort
+int* selectionSort(int* arr, int &size) {return arr;};
+
+bool sortingTest(int* arrToSort, int size)
+{
+	int* countingSorted = countingSort(arrToSort, size);
+	int* selectionSorted = selectionSort(arrToSort, size);
+
+	for (int i = 0; i < size; ++i) {
+		if (countingSorted[i] != selectionSorted[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
 	return 0;
 }
